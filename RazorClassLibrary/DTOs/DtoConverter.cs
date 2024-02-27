bd9737933e0b23a919beeb1d295bfc889e8dceb5
ethan.enger <ethan.enger@ENGR2KCP5S3>
2024-02-27 16:38:36 -0700

    Start of user service

---
+++ RazorClassLibrary/DTOs/DtoConverter.cs
@@ -0,0 +1,17 @@
+﻿using RazorClassLibrary.Data;
+
+namespace RazorClassLibrary.DTOs;
+
+public static class DtoConverter
+{
+    public static UserDTO ToDto(this EndUser user)
+    {
+        return new UserDTO()
+        {
+            Id = user.Id,
+            Guid = user.Guid,
+            Username = user.UserEmail,
+            Photo = user.Photo,
+        };
+    }
+}
