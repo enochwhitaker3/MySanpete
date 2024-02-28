18e6b4f7ef8db7efeca99831eb22876b4c37e117
ethan.enger <ethan.enger@ENGR2KCP5S3>
2024-02-27 17:32:37 -0700

    Role service

---
+++ RazorClassLibrary/Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using RazorClassLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Services;
+
+public interface IRoleService
+{
+    public Task<List<UserRole>> GetAllRoles();
+}
