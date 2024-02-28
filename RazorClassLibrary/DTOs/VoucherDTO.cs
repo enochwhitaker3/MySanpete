f2142a68f8167f5db2597d8581e75b646310c839
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-02-27 17:14:26 -0700

    Fixed voucher dto

--- RazorClassLibrary/DTOs/VoucherDTO.cs
+++ RazorClassLibrary/DTOs/VoucherDTO.cs
@@ -4,12 +4,12 @@ namespace RazorClassLibrary.DTOs;
 public class VoucherDTO
 {
     public int Id;
-    public required string BusinessName;
+    public string? BusinessName;
     public DateTime? StartDate;
     public DateTime? EndDate;
-    public required string PromoCode;
-    public required string PromoName;
-    public required string PromoDescription;
+    public string? PromoCode;
+    public string? PromoName;
+    public string? PromoDescription;
     public int? LeftInStock;
     public Decimal? RetailPrice;
     public int? AmmountReclaimable;
