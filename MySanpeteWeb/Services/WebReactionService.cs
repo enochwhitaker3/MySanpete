dc0fc01dd831fd992b4108f60dd4864cd4cd86e4
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-02-29 17:55:12 -0700

    Reaction Service Added

---
+++ MySanpeteWeb/Services/WebReactionService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySanpeteWeb.Data;
+using RazorClassLibrary.Data;
+using RazorClassLibrary.Requests;
+using RazorClassLibrary.Services;
+
+namespace MySanpeteWeb.Services;
+
+public class WebReactionService : IReactionService
+{
+    private IDbContextFactory<MySanpeteDbContext> dbContextFactory;
+    public WebReactionService(IDbContextFactory<MySanpeteDbContext> dbContextFactory)
+    {
+        this.dbContextFactory = dbContextFactory;
+    }
+    public async Task<BlogReaction> AddBlogReaction(AddReactionRequest request)
+    {
+        var context = await dbContextFactory.CreateDbContextAsync();
+
+        var reaction = await context.Reactions.FirstOrDefaultAsync(r => r.Unicode == request.Unicode);
+        var blog = await context.Blogs.FirstOrDefaultAsync(b => b.Id == request.ContentId);
+        var user = await context.EndUsers.FirstOrDefaultAsync(u => u.Guid == request.UserGuid);
+
+        if(reaction is null || blog is null || user is null)
+        {
+            throw new Exception("reaction or blog or user is null in the request");
+        }
+
+        var blogReaction = new BlogReaction()
+        {
+            ReactionId = reaction.Id,
+            BlogId = blog.Id,
+            UserId = user.Id,
+        };
+
+        return blogReaction;
+    }
+
+    public async Task<PodcastReaction> AddPodReaction(AddReactionRequest request)
+    {
+        var context = await dbContextFactory.CreateDbContextAsync();
+
+        var reaction = await context.Reactions.FirstOrDefaultAsync(r => r.Unicode == request.Unicode);
+        var podcast = await context.Podcasts.FirstOrDefaultAsync(p => p.Id == request.ContentId);
+        var user = await context.EndUsers.FirstOrDefaultAsync(u => u.Guid == request.UserGuid);
+
+        if (reaction is null || podcast is null || user is null)
+        {
+            throw new Exception("reaction or podcast or user is null in the request");
+        }
+
+        var podReaction = new PodcastReaction()
+        {
+            ReactionId = reaction.Id,
+            PodcastId = podcast.Id,
+            UserId = user.Id,
+        };
+
+        return podReaction;
+    }
+
+    public async Task<bool> DeleteBlogReaction(int id)
+    {
+        var context = await dbContextFactory.CreateDbContextAsync();
+
+        var doesExist = await context.BlogReactions.Where(b => b.Id == id).FirstOrDefaultAsync();
+        if (doesExist is null)
+        {
+            throw new Exception("No reactions found with given ID");
+        }
+
+        context.BlogReactions.Remove(doesExist);
+        await context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<bool> DeletePodReaction(int id)
+    {
+        var context = await dbContextFactory.CreateDbContextAsync();
+
+        var doesExist = await context.PodcastReactions.Where(p => p.Id == id).FirstOrDefaultAsync();
+        if (doesExist is null)
+        {
+            throw new Exception("No reactions found with given ID");
+        }
+
+        context.PodcastReactions.Remove(doesExist);
+        await context.SaveChangesAsync();
+
+        return true;
+    }
+}
---
+++ RazorClassLibrary/Requests/AddReactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Requests;
+
+public class AddReactionRequest
+{
+    public int ContentId { get; set; }
+    public string? Unicode { get; set; }
+    public Guid? UserGuid { get; set; }
+
+}
