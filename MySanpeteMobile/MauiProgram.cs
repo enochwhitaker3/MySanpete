948b579ccbee6bd72d6e1ec4e2a703eeb846a306
ethan.enger <ethan.enger@ENGR2KCP5S3>
2024-03-26 18:10:08 -0600

    Logging in for maui started

--- MySanpeteMobile/MauiProgram.cs
+++ MySanpeteMobile/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Auth0.OidcClient;
+using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 
 namespace MySanpeteMobile
@@ -22,6 +23,15 @@ namespace MySanpeteMobile
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
 #endif
+
+            builder.Services.AddSingleton(new Auth0Client(new()
+            {
+                Domain = builder.Configuration["Auth0:Domain"],
+                ClientId = builder.Configuration["Auth0:ClientId"],
+                RedirectUri = "myapp://callback/",
+                PostLogoutRedirectUri = "myapp://callback/",
+                Scope = "openid profile email",
+            }));
 
             return builder.Build();
         }
