330aad897068082910d061fcfe4c9cf95a76eebb
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-03-28 18:19:21 -0600

    Business Page Filtering Added

--- RazorClassLibrary/DTOs/VoucherDTO.cs
+++ RazorClassLibrary/DTOs/VoucherDTO.cs
@@ -5,6 +5,7 @@ public class VoucherDTO
 {
     public int Id;
     public string? BusinessName;
+    public byte[]? BusinessLogo;
     public DateTime? StartDate;
     public DateTime? EndDate;
     public string? PromoCode;
