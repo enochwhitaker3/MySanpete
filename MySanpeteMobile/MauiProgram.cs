6098d324bc9ea5d55d4207d103199dd22c089a77
ethan.enger <ethan.enger@ENGR2KCP5S3>
2024-04-04 13:10:00 -0600

    Maui authentication!

--- MySanpeteMobile/MauiProgram.cs
+++ MySanpeteMobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Auth0.OidcClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 
@@ -6,7 +7,6 @@ namespace MySanpeteMobile
 {
     public static class MauiProgram
     {
-        public static string domain { get; set; }
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -25,11 +25,12 @@ namespace MySanpeteMobile
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
 #endif
+            builder.Configuration.AddUserSecrets("147a395d-350b-47ec-852a-d7e5339b58dc");
 
             builder.Services.AddSingleton(new Auth0Client(new()
             {
-                Domain = "",
-                ClientId = "",
+                Domain = "dev-013fwxix4dwe1jea.us.auth0.com",
+                ClientId = "w7WZdACmKqbjV3Q5uIwvItVgJyUv4aQU",
                 RedirectUri = "myapp://callback",
                 PostLogoutRedirectUri = "myapp://callback",
                 Scope = "openid profile email",
---
+++ MySanpeteMobile/Services/MauiUserState.cs
@@ -0,0 +1,24 @@
+﻿using RazorClassLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySanpeteMobile.Services;
+
+public class MauiUserState : IUserState
+{
+    public ClaimsPrincipal User => throw new NotImplementedException();
+
+    public Task Login()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Logout()
+    {
+        throw new NotImplementedException();
+    }
+}
