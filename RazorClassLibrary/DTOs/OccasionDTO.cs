f1b08d4b7e6dff407d9ab747d650aab769db776b
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-04-15 17:04:34 -0600

    Getting occcasions photos end points

---
+++ RazorClassLibrary/DTOs/OccasionDTO.cs
@@ -0,0 +1,30 @@
+﻿using RazorClassLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.DTOs;
+
+public class OccasionDTO
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public decimal? XCoordinate { get; set; }
+
+    public decimal? YCoordinate { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public int BusinessId { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? PhotoURL { get; set; }
+
+}
---
+++ RazorClassLibrary/Requests/UpdateOccasionRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Requests;
+
+public class UpdateOccasionRequest
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public decimal? XCoordinate { get; set; }
+
+    public decimal? YCoordinate { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public int BusinessId { get; set; }
+
+    public string? Description { get; set; }
+
+    public byte[]? Photo { get; set; }
+}
