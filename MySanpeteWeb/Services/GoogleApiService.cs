ccb50fdcc29fcd45dca3a59db0f2fc5cf83cca8f
ethan.enger <ethan.enger@ENGR2KCP5S3>
2024-04-16 17:55:41 -0600

    Formating fixed

--- MySanpeteWeb/Services/GoogleApiService.cs
+++ MySanpeteWeb/Services/GoogleApiService.cs
@@ -9,9 +9,9 @@ public class GoogleApiService(IConfiguration config) : IGoogleApiService
     {
         var client = new HttpClient();
         var response = await client.GetFromJsonAsync<Address>($"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={config.GetValue<string>("Google_Api")}");
-        if(response is not null)
+        if (response is not null)
         {
-            return new Coords() {X = response!.results!.First()!.geometry!.location!.lat, Y = response!.results!.First()!.geometry!.location!.lng };
+            return new Coords() { X = response!.results!.First()!.geometry!.location!.lat, Y = response!.results!.First()!.geometry!.location!.lng };
         }
         return null;
     }
