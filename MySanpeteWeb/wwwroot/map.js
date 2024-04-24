b3e800ebcf2632fde4bb17bfe29e8e25a2c5d75e
enochwhitaker3 <enochwhitaker3@gmail.com>
2024-04-24 15:31:50 -0600

    Fixed User Profile Updating, Text on Home Screen, Javascript on Map, and a bunch of small changes to styles

--- MySanpeteWeb/wwwroot/map.js
+++ MySanpeteWeb/wwwroot/map.js
@@ -6,31 +6,22 @@ function initializeMap(mapsKey, jsonOccasions, jsonBusinesses) {
     // Create the map instance
     map = new atlas.Map("mapDiv", {
         view: "Auto",
-        center: [-111.58251375208721, 39.36257310977088], 
+        center: [-111.58251375208721, 39.36257310977088],
         zoom: 13,
         showFeedbackLink: false,
         showLogo: false
     });
 
-    // Get user's location
-    //navigator.geolocation.getCurrentPosition(function (position) {
-    //    var userLocation = [- 111.58251375208721, 39.36257310977088];
-    //    map.setCamera({
-    //        center: userLocation,
-    //        zoom: 13
-    //    });
-    //});
+
 
     map.setCamera({
         center: [-111.58251375208721, 39.36257310977088],
         zoom: 13
     });
 
+    var deserializedOccasions = JSON.parse(jsonOccasions);
 
-
-    var deserializedData = JSON.parse(jsonOccasions);
-
-    deserializedData.forEach((item) => {
+    deserializedOccasions.forEach((item) => {
         var marker = new atlas.HtmlMarker({
             color: 'DodgerBlue',
             text: 'O',
@@ -42,34 +33,40 @@ function initializeMap(mapsKey, jsonOccasions, jsonBusinesses) {
         });
         map.markers.add(marker);
 
+        const popupManager = map.popups;
+
         map.events.add('click', marker, () => {
+            popupManager.clear();
             marker.togglePopup();
         });
-    })
+    });
 
-    //Add a click event to toggle the popup.
-    var deserializedData = JSON.parse(jsonBusinesses);
+    var deserializedBusinesses = JSON.parse(jsonBusinesses);
 
-    deserializedData.forEach((item) => {
-        var marker = new atlas.HtmlMarker({
-            color: 'DodgerBlue',
-            text: 'B',
-            position: [item.YCoordinate, item.XCoordinate],
-            popup: new atlas.Popup({
-                content: `<div style="padding:10px">${item.BusinessName}</div>`,
-                pixelOffset: [0, -30]
-            })
-        });
-
+    deserializedBusinesses.forEach((item) => {
         if (item.YCoordinate !== null || item.XCoordinate !== null) {
+            var marker = new atlas.HtmlMarker({
+                color: 'DodgerBlue',
+                text: 'B',
+                position: [item.YCoordinate, item.XCoordinate],
+                popup: new atlas.Popup({
+                    content: `<div style="padding:10px">${item.BusinessName}</div>`,
+                    pixelOffset: [0, -30]
+                })
+            });
+
             map.markers.add(marker);
 
+
+            const popupManager = map.popups;
+
+
             map.events.add('click', marker, () => {
+                popupManager.clear();
                 marker.togglePopup();
             });
         }
-    })
-
+    });
 }
 
 function focusMap(YCoordinate, XCoordinate) {
