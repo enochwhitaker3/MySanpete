d7434de9dfbda5f068bc25a837f24127c6c90033
dustyshaw <shustydaw@gmail.com>
2024-04-24 18:20:19 -0600

    Added edit blog test. Not sure how else to test it...

---
+++ MySanpeteTests/UnitTests/AdminEditBlogTests.cs
@@ -0,0 +1,54 @@
+﻿using MySanpeteWeb.Components.Pages.AdminPages;
+using RazorClassLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySanpeteTests.UnitTests
+{
+    public class AdminEditBlogTests
+    {
+        [Fact]
+        public void CanCallMethod()
+        {
+            var page = SetUpPage();
+
+            var NewBlog = new BlogDTO()
+            {
+
+                Title = "Blog Title",
+                Content = "<h1>Title</h1><p>blah blahh</p>",
+                AuthorName = "Dusty Shaw",
+                PublishDate = DateTime.Now,
+                Commentable = true,
+                PhotoURL = "/MySanpete.jpg"
+
+            };
+
+            page.SelectBlog(NewBlog);
+
+
+        }
+
+        public AdminEditBlog SetUpPage() 
+        {
+            var page = new AdminEditBlog();
+
+            var Blog = new BlogDTO()
+            {
+
+                Title = "Blog Title",
+                Content = "<h1>Title</h1><p>blah blahh</p>",
+                AuthorName = "Dusty Shaw",
+                PublishDate = DateTime.Now,
+                Commentable = true,
+                PhotoURL = "/MySanpete.jpg"
+
+            };
+            page.SelectedBlog = Blog;
+            return page;
+        }
+    }
+}
